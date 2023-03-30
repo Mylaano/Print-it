f51b49276803b7981a54bd5f176b42a22f733bbb
Damien DRUAUX <druaux21@outlook.fr>
2023-03-30 19:47:17 +0200

    Update script.js
    - Final commit

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,62 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// Ajout des événements pour les flèches de gauche et droite
+
+const 
+	arrowLeft = document.querySelector(".arrow_left"),
+	arrowRight = document.querySelector(".arrow_right");
+
+let dotSelected = 0;
+
+function slideImage(dotId) {
+	const bannerImg = document.querySelector(".banner-img");
+	bannerImg.src = "./assets/images/slideshow/" + slides[dotId]['image'];
+
+	const bannerId = document.getElementById("banner");
+	const text = bannerId.getElementsByTagName("p");
+	text[0].innerHTML = slides[dotId]['tagLine'];
+}
+
+arrowLeft.addEventListener('click', () => {
+	const test = document.querySelectorAll(".dot");
+
+	test[dotSelected].classList.remove("dot_selected");
+	dotSelected--;
+
+	if(dotSelected < 0) {
+		dotSelected = 3;
+	}
+
+	test[dotSelected].classList.add("dot_selected");
+
+	slideImage(dotSelected);
+})
+
+arrowRight.addEventListener('click', () => {
+	const test = document.querySelectorAll(".dot");
+
+	test[dotSelected].classList.remove("dot_selected");
+	dotSelected++;
+
+	if(dotSelected > 3) {
+		dotSelected = 0;
+	}
+
+	test[dotSelected].classList.add("dot_selected");
+
+	slideImage(dotSelected);
+})
+
+// Ajouts des bullets points
+
+const dotContenair = document.querySelector(".dots");
+
+for(let i = 0; i < slides.length; i++) {
+	let divDot = dotContenair.appendChild(document.createElement('div'));
+	divDot.classList.add("dot");
+}
+
+const firstDot = document.querySelector(".dot");
+firstDot.classList.add("dot_selected");
